2a8ed71c6a2ebbd9acd8338f8ee33dd55ef992f8
mpaulson <the.primeagen@gmail.com>
2024-01-13 16:00:53 -0700

    feat: thinking about how to calculate apm

--- lua/vim-apm/reporter/calculator.lua
+++ lua/vim-apm/reporter/calculator.lua
@@ -1,8 +1,22 @@
 local motion_parser = require("vim-apm.reporter.motion_parser")
 
+local Frequency = {}
+Frequency.__index = Frequency
+
+function Frequency.new(max)
+    return setmetatable({
+        motions = {},
+        index = 1,
+        max = max or 10,
+    }, Frequency)
+end
+
+function Frequency:push(motion)
+end
+
 ---@class APMAggregateMotionValue
 ---@field count number
----@field timings_total number[]
+---@field timings_total number
 
 ---@class APMCalculator
 ---@field motions table<string, APMAggregateMotionValue>
@@ -11,7 +25,8 @@ local motion_parser = require("vim-apm.reporter.motion_parser")
 ---@field insert_times number
 ---@field insert_times_count number
 ---@field buf_enter_count number
----@field mode table<string, number>
+---@field mode_times table<string, number>
+---@field state string
 local Calculator = {}
 Calculator.__index = Calculator
 
@@ -23,7 +38,8 @@ function Calculator.new()
         insert_times = 0,
         insert_times_count = 0,
         buf_enter_count = 0,
-        mode = {},
+        mode_times = {},
+        state = "",
     }, Calculator)
 end
 
@@ -43,8 +59,43 @@ end
 ---@param motion APMMotionItem
 function Calculator:motion(motion)
     local key = motion_parser.generate_motion_key(motion.chars)
-    local parts = motion_parser.parse_motion_parts(motion.chars)
+    local sum = 0
+    for _, timing in ipairs(motion.timings) do
+        sum = sum + timing
+    end
 
-    self.motions
+    self.motions[key] = self.motions[key] or {
+        count = 1,
+        timings_total = sum
+    }
+    self.motions[key].count = self.motions[key].count + 1
+    self.motions[key].timings_total = self.motions[key].timings_total + sum
 end
 
+---@param mode string
+function Calculator:mode(mode)
+    self.mode_times[mode] = (self.mode_times[mode] or 0) + 1
+end
+
+function Calculator:buf_enter()
+    self.buf_enter_count = self.buf_enter_count + 1
+end
+
+function Calculator:write()
+    self.write_count = self.write_count + 1
+end
+
+---@param insert_time number
+function Calculator:insert_time(insert_time)
+    self.insert_times = self.insert_times + insert_time
+    self.insert_times_count = self.insert_times_count + 1
+end
+
+--- this is a placeholder for when i have navigating / editing / idle states
+--- as of now, it does nothing to the calculator
+---@param state string
+function Calculator:apm_state(state)
+    self.state = state
+end
+
+return Calculator
