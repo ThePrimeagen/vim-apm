1fda0d873edc2f7ed8b542a45dc07c864a972d3e
mpaulson <the.primeagen@gmail.com>
2024-01-12 20:35:22 -0700

    feat: testing the complex motion

--- pkg/motions/motion_test.go
+++ pkg/motions/motion_test.go
@@ -15,3 +15,17 @@ func TestParseSimpleMotion(t *testing.T) {
 	assert.Equal(t, 9, count)
 	assert.Equal(t, "j", motion)
 }
+
+func TestParseComplexMotion(t *testing.T) {
+	m, err := motions.Parse("6d9j")
+	assert.Nil(t, err)
+
+	count, motion := m.GetCommand()
+
+	assert.Equal(t, 6, count)
+	assert.Equal(t, "d", motion)
+
+	count, motion = m.GetMotion()
+	assert.Equal(t, 9, count)
+	assert.Equal(t, "j", motion)
+}
